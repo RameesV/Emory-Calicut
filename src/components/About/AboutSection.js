d93d7fcd6b3041353f3d6c7a417bc1b47e7d53c8
RameesMhd <muhammed.ramees@neopraxis.in>
2024-08-20 07:35:17 +0530

    - package changes - deleted unwanted sample files - Homepage about section changes

--- src/components/About/AboutSection.js
+++ src/components/About/AboutSection.js
@@ -8,12 +8,13 @@ const AboutSection = () => {
         <>
             <div className="content-1 py-5">
                 <div className="container py-md-5 HomePageAbout">
+                    <div xs={12} md={6} lg={6} className="content-1-left forms-25-info">
+                        < HeaderComponent mainHeading={"Learn at Your Own Place"} subHeading={"About Us"} />
+                    </div>
                     <Row className="row content-1-grids">
-                        <Col xs={12} md={6} lg={6} className="content-1-left forms-25-info">
-                            < HeaderComponent mainHeading={"Learn at Your Own Place"} subHeading={"About Us"} />
-                        </Col>
-                        <Col xs={12} md={6} lg={6} className="content-1-right">
-                            <p className="">
+
+                        <Col xs={12} md={8} lg={8} className="content-1-right">
+                            <p className="about-desc mb-5">
                                 <span className="font-weight-bold">Education</span> in taking a new sense of direction
                                 and Perception. EMORY Educational institution
                                 impact the new mode of quality education of
@@ -21,10 +22,7 @@ const AboutSection = () => {
                                 Experience and reputed professionals and
                                 their successful journey resulted in the
                                 establishment of emory educational institution.
-                            </p>
-                        </Col>
-                        <Col xs={12} md={6} lg={6} className="pl-sm-5 content-1-left ">
-                            <p className="">
+
                                 we aim at the experimental Learning of the
                                 students and the empowerment of rural
                                 education the programs provided at emory
@@ -37,9 +35,9 @@ const AboutSection = () => {
                                 emphasis on the skill based and service
                                 oriented molding of the students.
                             </p>
-                            <Link className="card-link" smooth={true} to="about">Read more</Link>{' '}
+                            <Link className="card-link" to="about">Read more</Link>
                         </Col>
-                        <Col xs={12} sm={6} md={6} className="about-us-image-wrapper">
+                        <Col xs={12} sm={6} md={4} lg={4} className="about-us-image-wrapper p-4">
                             <img alt="loading..." className="about-us-image" src="https://sadectip.sirv.com/React%20Project%20Files/webeducation.png" />
                         </Col>
                     </Row>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './components/Home/Home';

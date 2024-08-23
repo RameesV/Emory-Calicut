9d049714d00ec76f6f8cac8b3a8f5b2baa08cdef
RameesMhd <muhammed.ramees@neopraxis.in>
2024-08-24 00:18:51 +0530

    New reviews added with photose

--- src/components/MiniComponents/Testimonials/Testimonials.js
+++ src/components/MiniComponents/Testimonials/Testimonials.js
@@ -2,43 +2,49 @@ import React from 'react'
 import './Testimonials.scss';
 import HeaderComponent from '../HeaderComponent/HeaderComponent';
 import SlickSlider from '../SlickSlider/SlickSlider';
+import dilshan from '../../../assets/Reviews/r1.jpg';
+import nafiya from '../../../assets/Reviews/r2.jpg';
+import asmina from '../../../assets/Reviews/r3.jpg';
+import shameela from '../../../assets/Reviews/r4.jpg';
+import sajva from '../../../assets/Reviews/r5.jpg';
+
 
 function Testimonials() {
     const testimonials = [
         {
             id: 1,
-            name: 'John Doe',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, fugit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, fugit.',
-            image: 'https://wp.w3layouts.com/digitaledu/wp-content/themes/digitaledu/assets/images/team2.jpg',
-            role: 'student'
+            name: 'Dilshana Sherin',
+            description: 'The Institute is well reputed and its teachers offer a commendable educational experience. Their commitment to providing clear, practical knowledge is evident, and their supportive approach greatly enhances the learning environment.',
+            image: dilshan,
+            role: 'Student'
         },
         {
             id: 2,
-            name: 'Tommy sakura',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, fugit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, fugit.',
-            image: 'https://wp.w3layouts.com/digitaledu/wp-content/themes/digitaledu/assets/images/team3.jpg',
-            role: 'student'
+            name: 'Nafiya pk',
+            description: 'I had the pleasure of being a student at Emory Institute, specifically in the hospital administration program, and I must say it was an exceptional experience. The overall atmosphere  which made learning and growth a truly enjoyable process',
+            image: nafiya,
+            role: 'Student'
         },
         {
             id: 3,
-            name: 'Gloria smith',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, fugit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, fugit.',
-            image: 'https://wp.w3layouts.com/digitaledu/wp-content/themes/digitaledu/assets/images/team4.jpg',
-            role: 'student'
+            name: 'Asmina',
+            description: 'Emory institute is very reliable for professional course.They are giving such a good and hands on training.They especially provide job oriented training.Their way of teaching is good',
+            image: asmina,
+            role: 'Student'
         },
         {
             id: 4,
-            name: 'John Black',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. VelitLorem ipsum dolor sit amet consectetur adipisicing elit. Velit, fugit. Lorem ipsum dolor ',
-            image: 'https://wp.w3layouts.com/digitaledu/wp-content/themes/digitaledu/assets/images/team5.jpg',
-            role: 'student'
+            name: 'Shameela',
+            description: 'The teaching way of this institution is good.internship rate are also good. Facualty members of institution are also very well qualified and always helpful.',
+            image: shameela,
+            role: 'Student'
         },
         {
-            id: 4,
-            name: 'John Black',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. VelitLorem ipsum dolor sit amet consectetur adipisicing elit. Velit, fugit. Lorem ipsum dolor ',
-            image: 'https://wp.w3layouts.com/digitaledu/wp-content/themes/digitaledu/assets/images/team5.jpg',
-            role: 'student'
+            id: 5,
+            name: 'Sajva',
+            description: 'The tutors are very knowledgeable and well-informed. They were really supportive throughout the course.',
+            image: sajva,
+            role: 'Student'
         },
     ];
 
